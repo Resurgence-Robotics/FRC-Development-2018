ddda851241c8633c803e7778bf708c333e8a3414
DESKTOP-6KVRRNH--Morgan <mcbitbyte@gmail.com>
2017-08-05 15:06:49 -0400

    Revert "Initialized repository and released vision project"
    This reverts commit 9af13588a1d3b85eec7c916000ebfa72a40b7b41.

--- OpenCV_tegra/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_calib3d320 -lopencv_core320 -lopencv_features2d320 -lopencv_flann320 -lopencv_highgui320 -lopencv_imgcodecs320 -lopencv_imgproc320 -lopencv_ml320 -lopencv_objdetect320 -lopencv_photo320 -lopencv_shape320 -lopencv_stitching320 -lopencv_superres320 -lopencv_video320 -lopencv_videoio320 -lopencv_videostab320 -lopencv_ffmpeg320_64
-
