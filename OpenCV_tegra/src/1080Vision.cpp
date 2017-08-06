511531fcd51f5012a29947eb75032d00637b4a05
DESKTOP-6KVRRNH--Morgan <mcbitbyte@gmail.com>
2017-08-06 14:59:28 -0400

    added some variables for images and properties

--- OpenCV_tegra/src/1080Vision.cpp
+++ OpenCV_tegra/src/1080Vision.cpp
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include <iostream>
-#include <sstream>
-#include <unistd.h>
+#include <stdio.h>	// standard input and output stream
+#include <stdlib.h>	// standard library for system level functions and low level application access
+#include <string> 	//
+#include <iostream>	// linux gcc application capabilities
+#include <sstream>	// linux gcc runtime access
+#include <unistd.h>	// universal standard numeric rules
 //opencv includes
-#include <opencv2/opencv.hpp>
-#include "opencv2/highgui.hpp"
+#include <opencv2/opencv.hpp>// all C++ libs for opencv algorithms
+#include <opencv2/core.hpp>// data types specific to images and matrix functionality
 
 using namespace cv;
 using namespace std;
@@ -18,16 +18,21 @@ using namespace std;
  */
 // functions to override
 //Camera constants used for distance calculation
-#define X_IMAGE_RES 320		//X Image resolution in pixels, should be 160, 320 or 640
+#define X_IMAGE_RES 320		//X Image resolution in pixels, should be 160, 320 or 640 Y image resolution is double for 2:1 aspect ratio
 #define Y_IMAGE_RES 640
 #define VIEW_ANGLEH 61		//MICROSOFT LIFECAM HD-5000
-#define VIEW_ANGLEV - 34.3
+#define VIEW_ANGLEV 34.3	//MICROSOFT LIFECAM HD-5000
 //#define VIEW_ANGLE 43.5  //Axis M1011 camera -- OUR IP CAM FROM KOP
 
-//CLASSIFICATION OF THE OBJECT WE ARE PROCESSING used for target identification *** unit = pixel ***
-//#define ASPECT_RATIO 2 // FOR RECTANGULAR OBJECTS only
+//CLASSIFICATION OF THE OBJECT WE ARE PROCESSING used for target identification
+//							*** units = pixel ***
+#define ASPECT_RATIO_CONST 2.75 // FOR RECTANGULAR OBJECTS ONLY, TARGET WIDTH TO HIGHT RATIO
 #define X_EDGE_LIMIT 3
 #define Y_EDGE_LIMIT 4
+#define CAM_ID 0
+#define CAM_IP 10.10.80.250
+string OriginalImage ("C:/Users/Public/Pictures/Original.jpg"); // will throw escape sequences in linux
+//string HSVfilteredImage ("C:\Users\Public\Pictures\Filters.jpg");// in case we need a better starting point
 
 //Minimum area of particles to be considered
 #define AREA_MINIMUM 500// 500 pixels
@@ -48,24 +53,33 @@ int HSV[6] { 60, 100, 90, 255, 20, 255};	//HSV threshold criteria array, ranges 
 
 	};
 
-	//TODO: start a camera stream and display an image from the while loop
+
 int main()
 {
-	//create vcideostream object
+	//create videostream object and image
+	// from stream
+	VideoCapture Stream;
+	Stream= VideoCapture(CAM_ID);//starts a camera stream
+	//from image
+	Mat Original ;
+	Original= imread(OriginalImage);// loads an image from the file
+	//Original= imread()
 	// create windows for viewing each step in filter process until we are able to run without them ie... "calibrated"
 
 	while(true)//change later to something like "while an image exists"
 	{
 
-	//grab one frame from camera
-
+	//grab one image from file or stream
+		Mat Frame;
+		Frame= Stream.grab();// from usb
+		//Frame= Original;// from filesystem
 	//process it-> WHEN EACH OF THE BELOW STEPS IS COMPLETE THEY SHOULD HAVE A CORRESPONDING WINDOW FOR PREVIEWS.
 		//HSV filter
 		printf("Threshold: %i, %i, %i, %i, %i, %i, \n", HSV[0], HSV[1], HSV[2], HSV[3], HSV[4], HSV[5]  );// how to use arrays
 		//canny edge detect edges within the object
 		//convex hull the image
 		//remove any smaller particles
-		//perform algorithms described later to score and classify the object
+	//perform algorithms described later to score and classify the object
 	}
 
 	return 0;
