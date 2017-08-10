c66d40e4882b4e14817df4373c0ab398b9e2dbec
DESKTOP-6KVRRNH--Morgan <mcbitbyte@gmail.com>
2017-08-09 21:55:30 -0400

    added functions void PrintVector(TContours &input) void GetContours(Mat &input, TContours &contours) void FilterContours(TContours &input, TContours &output)

--- OpenCV_tegra/Documentation/GripPipeline.cpp
+++ OpenCV_tegra/Documentation/GripPipeline.cpp
@@ -20,6 +20,9 @@ void GripPipeline::Process(cv::Mat& source0){
 	cv::Mat findContoursInput = hsvThresholdOutput;
 	bool findContoursExternalOnly = true;  // default Boolean
 	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
+
+
+
 	//Step Filter_Contours0:
 	//input
 	std::vector<std::vector<cv::Point> > filterContoursContours = findContoursOutput;
@@ -118,18 +121,19 @@ std::vector<std::vector<cv::Point> >* GripPipeline::GetConvexHullsOutput(){
 	void GripPipeline::filterContours(std::vector<std::vector<cv::Point> > &inputContours, double minArea, double minPerimeter, double minWidth, double maxWidth, double minHeight, double maxHeight, double solidity[], double maxVertexCount, double minVertexCount, double minRatio, double maxRatio, std::vector<std::vector<cv::Point> > &output) {
 		std::vector<cv::Point> hull;
 		output.clear();
-		for (std::vector<cv::Point> contour: inputContours) {
+		for (std::vector<cv::Point> contour: inputContours)
+		{
 			cv::Rect bb = boundingRect(contour);
 			if (bb.width < minWidth || bb.width > maxWidth) continue;
 			if (bb.height < minHeight || bb.height > maxHeight) continue;
-			double area = cv::contourArea(contour);
+		double area = cv::contourArea(contour);
 			if (area < minArea) continue;
 			if (arcLength(contour, true) < minPerimeter) continue;
 			cv::convexHull(cv::Mat(contour, true), hull);
-			double solid = 100 * area / cv::contourArea(hull);
+		double solid = 100 * area / cv::contourArea(hull);
 			if (solid < solidity[0] || solid > solidity[1]) continue;
 			if (contour.size() < minVertexCount || contour.size() > maxVertexCount)	continue;
-			double ratio = (double) bb.width / (double) bb.height;
+		double ratio = (double) bb.width / (double) bb.height;
 			if (ratio < minRatio || ratio > maxRatio) continue;
 			output.push_back(contour);
 		}
--- OpenCV_tegra/src/1080Vision.cpp
+++ OpenCV_tegra/src/1080Vision.cpp
@@ -16,6 +16,7 @@ using namespace std;
  *	Author: 1080 Resurgence Robotics
  *  Created on: Jul 30, 2017
  */
+
 // functions to override
 //Camera constants used for distance calculation
 #define X_IMAGE_RES 320		//X Image resolution in pixels, should be 160, 320 or 640 Y image resolution is double for 2:1 aspect ratio
@@ -28,7 +29,8 @@ using namespace std;
 #define CAM_IP 10.10.80.250 // ip camera
 string OriginalImage ("Samples/Original.png"); // test sample images
 string HSVfilteredImage ("Samples/HSVThresholdOutput.png");
-
+string ContoursImage ("Samples/ContoursImage.png");
+string filteredImage("Samples/ContoursImage.png");
 //CLASSIFICATION OF THE OBJECT WE ARE PROCESSING used for target identification
 //							*** units = pixel ***
 #define ASPECT_RATIO_CONST 2.75 // FOR RECTANGULAR OBJECTS ONLY, TARGET WIDTH TO HIGHT RATIO
@@ -55,8 +57,11 @@ double Val[] {60, 255};
 		double yEdge;
 
 	};
+	typedef vector<vector<Point> > TContours;
+	typedef vector<Point> UContour;
 void HSVThreshold(Mat &input, Mat &output,double Hue[],double Sat[], double Val[]);//created above main so it can be used, but defined later to keep from using up space
-void FindContours(Mat &input, vector<vector<Point> > &contours);
+void GetContours(Mat &input, TContours &contours);
+void FilterContours(TContours &input, TContours &output);
 int main()
 {
 	//create videostream object and image
@@ -72,39 +77,88 @@ int main()
 	{
 
 	//grab one image from file or stream
-		Mat Frame(X_IMAGE_RES, Y_IMAGE_RES, CV_CN_MAX);// create a matrix of pixil data called frame THAT MATCHES CAMERA RESOLUTION
+		Mat frame(X_IMAGE_RES, Y_IMAGE_RES, CV_CN_MAX);// create a matrix of pixil data called frame THAT MATCHES CAMERA RESOLUTION
 		//frame= Stream.grab();// from usb, grab a frame if new frame is available
-		Frame= Original;// from filesystem
+		frame= Original;// from filesystem
 	// WHEN EACH OF THE BELOW STEPS IS COMPLETE, create a window or new image for validation.
 	//HSV filter
-		Mat HSVThresholdOutput(X_IMAGE_RES, Y_IMAGE_RES, CV_CN_MAX);// 2 channel
-		HSVThreshold(Frame, HSVThresholdOutput,Hue,Sat,Val);// each step of the process should be like this
-		imwrite(HSVfilteredImage,HSVThresholdOutput);// create a new image to show the processing worked
+		Mat HSVThresholdOutput(X_IMAGE_RES, Y_IMAGE_RES, CV_8UC1);// 1 channel binary
+		HSVThreshold(frame, HSVThresholdOutput,Hue,Sat,Val);// each step of the process should be like this
 	//identify contours
-		Mat ContoursOutput;
-	// filter thew contours we want by pixel area
+		TContours contours; //created empty vector
+		GetContours(HSVThresholdOutput, contours);
+		PrintVector(contours);
+	//Filter contours
+//
+//		TContours finalContours;
+//		FilterContours(contours, finalContours);
 
-	//convex hull the image
 
-//perform algorithms described later to score and classify the contours.
-		//sleep(.05);//20fps--not a real measurement
+
+	//	waitKey();// waits to exit program until we have pressed a key
 		break;
 	}
 	return 0;
 }
-
-void HSVThreshold(Mat &input, Mat &output,double Hue[],double Sat[], double Val[])// this function uses an input and output givin by the user allowing major flexibility
+void PrintVector(TContours &input)
+{
+	for (int i= 0; i< input.end(); i++)
+	{
+		printf("%i, ", input[i]);
+	}
+}
+void HSVThreshold(Mat &input, Mat &output, double Hue[], double Sat[], double Val[])// this function uses an input and output givin by the user allowing major flexibility
 {
 	cvtColor(input,output,cv::COLOR_BGR2HSV);// convert the image from color image channles to 2 channel HSV "black white", binary
 	inRange(output, cv::Scalar(Hue[0],Sat[0],Val[0]),cv::Scalar(Hue[1],Sat[1],Val[1]),output);
+	imwrite(HSVfilteredImage,output);// create a new image to show the processing worked
 }
 
-void FindContours(Mat &input, vector<vector<Point> > &contours) {
-	vector<Vec4i> hierarchy;
-	contours.clear();
-	cv::findContours(input, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-}
-void FilterContours()
+void GetContours(Mat &input, TContours &contours)
 {
+	Mat CannyOutput;
+	Canny(input, CannyOutput, X_IMAGE_RES, Y_IMAGE_RES);
+	vector<Vec4i> hierarchy;
+	findContours(CannyOutput, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	imwrite(ContoursImage, CannyOutput);
+}
 
+void FilterContours(TContours &input, TContours &output)
+{
+	double minArea = 300.0;  // default Double
+	double minPerimeter = 0.0;  // default Double
+	double minWidth = 0.0;  // default Double
+	double maxWidth = 1000.0;  // default Double
+	double minHeight = 0.0;  // default Double
+	double maxHeight = 1000.0;  // default Double
+	double solidity[] = {20, 100};
+	double maxVertices = 1000000.0;  // default Double
+	double minVertices = 0.0;  // default Double
+	double minRatio = 0.0;  // default Double
+	double maxRatio = 1000.0;  // default Double
+	TContours hull;
+	UContour shape;
+	output.clear();
+	Mat OutputImage(X_IMAGE_RES, Y_IMAGE_RES, CV_8UC1);
+	vector<Vec4i> hierarchy;
+
+	for (UContour contour: input)
+			{
+				Rect bb = boundingRect(input);
+				if (bb.width < minWidth || bb.width > maxWidth) continue;
+				if (bb.height < minHeight || bb.height > maxHeight) continue;
+				double area = contourArea(input);
+				if (area < minArea) continue;
+				if (arcLength(input, true) < minPerimeter) continue;
+				convexHull(Mat(input, true), hull);
+				double solid = 100 * area / contourArea(hull);
+				if (solid < solidity[0] || solid > solidity[1]) continue;
+				if (input.size() < minVertices || input.size() > maxVertices)	continue;
+				double ratio = (double) bb.width / (double) bb.height;
+				if (ratio < minRatio || ratio > maxRatio) continue;
+				output.push_back(contour);
+
+			}
+			drawContours(OutputImage, hull, -1, 255, CV_FILLED);
+			imshow(filteredImage, OutputImage);
 }
